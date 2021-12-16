cb9c4228d0a3467f9f0b940c7a00e37fb657917b
chaxiaowu95 <104088657+chaxiaowu95@users.noreply.github.com>
2021-12-16 20:58:15 +0000

    chore(package): update babel-cli to version 6.7.7

--- src/freeze_graph.sh
+++ src/freeze_graph.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 $model_type=$1
 python freeze_graph.py --input_graph=../weights/$model_type/graph.pb \
-    --input_checkpoint=../weights/$model_type/mode
+    --input_checkpoint=../weights/$model_type/model.c
