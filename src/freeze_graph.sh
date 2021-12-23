f6112ead92135126282e7acf428cfc17371b9ccd
chaxiaowu95 <104088657+chaxiaowu95@users.noreply.github.com>
2021-12-23 10:27:12 +0000

    Fix mode

--- src/freeze_graph.sh
+++ src/freeze_graph.sh
@@ -2,4 +2,5 @@
 $model_type=$1
 python freeze_graph.py --input_graph=../weights/$model_type/graph.pb \
     --input_checkpoint=../weights/$model_type/model.checkpoint \
-    --output_graph=../weights/$model_type/freeze_graph
+    --output_graph=../weights/$model_type/freeze_graph.pb \
+    -
