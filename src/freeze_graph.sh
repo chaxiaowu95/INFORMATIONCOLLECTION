f4b0590d89b2b1eed6c839a088bd59eab5bd6178
chaxiaowu95 <104088657+chaxiaowu95@users.noreply.github.com>
2021-12-25 12:27:47 +0000

    ceph: have get_authorizer methods return pointers

--- src/freeze_graph.sh
+++ src/freeze_graph.sh
@@ -3,4 +3,4 @@ $model_type=$1
 python freeze_graph.py --input_graph=../weights/$model_type/graph.pb \
     --input_checkpoint=../weights/$model_type/model.checkpoint \
     --output_graph=../weights/$model_type/freeze_graph.pb \
-    --output_node_n
+    --output_node_name
