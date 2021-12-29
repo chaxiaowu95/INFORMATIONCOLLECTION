154810aa1a78cb26e8d0ecb18e25088b65f5dab0
chaxiaowu95 <104088657+chaxiaowu95@users.noreply.github.com>
2021-12-29 08:45:39 +0000

    patch bump

--- src/main.py
+++ src/main.py
@@ -2,3 +2,9 @@
 
 import sys
 import numpy as np
+
+import utils
+from model import LogisticRegression, DNN, RankNet, LambdaRank
+from prepare_data import label_file_pat, group_file_pat, feature_file_pat
+
+def load_data(type):
