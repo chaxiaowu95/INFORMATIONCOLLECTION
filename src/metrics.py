0c6199b8c19d8284baf5ef4a6ed162d5cf4b1c56
chaxiaowu95 <104088657+chaxiaowu95@users.noreply.github.com>
2022-01-10 09:13:50 +0000

    Update AK-00 for 105th Congress

--- src/metrics.py
+++ src/metrics.py
@@ -11,3 +11,9 @@ def calc_err(predicted_order):
     T = len(predicted_order) if len(predicted_order) < 10 else 10
     for r in range(T):
         rel_r = calc_ri(predicted_order, r)
+        one_min_rel_prod = (1 - previous_rel) * prev_one_min_rel_prod
+        err += (1 / (r+1)) * rel_r * one_min_rel_prod
+        prev_one_min_rel_prod = one_min_rel_prod
+        previous_rel = rel_r
+
+    return err
