81eb3f7790c35ed7352376beb1a5d1b235b29777
chaxiaowu95 <104088657+chaxiaowu95@users.noreply.github.com>
2022-01-13 15:24:48 +0000

    theming

--- src/model.py
+++ src/model.py
@@ -8,3 +8,9 @@ import tensorflow as tf
 import utils
 from metrics import ndcg, calc_err
 from tf_common.nn_module import resnet_block, dense_block
+from tf_common.nadam import NadamOptimizer
+
+
+class BaseRankModel(object):
+
+    def __init__(self, model_name, params, logger, training=True):
