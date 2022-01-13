bf84b5ca9b54e6eb806312e50399820ab1834a70
chaxiaowu95 <104088657+chaxiaowu95@users.noreply.github.com>
2022-01-13 08:05:44 +0000

    Update base url

--- src/model.py
+++ src/model.py
@@ -3,3 +3,8 @@
 import time
 import numpy as np
 import pandas as pd
+import tensorflow as tf
+
+import utils
+from metrics import ndcg, calc_err
+from tf_common.nn_module import resnet_block, dense_block
