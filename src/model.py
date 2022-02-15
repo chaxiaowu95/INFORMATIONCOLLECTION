ca03b9d46dcb67c7c357dd5f53632b23d2158c3b
chaxiaowu95 <104088657+chaxiaowu95@users.noreply.github.com>
2022-02-15 08:25:12 +0000

    Fixes #97

--- src/model.py
+++ src/model.py
@@ -293,3 +293,9 @@ class LogisticRegression(DNN):
     def _score_fn_inner(self, x, reuse=False):
         score = tf.layers.dense(x, 1, activation=None,
                                 kernel_initializer=tf.glorot_uniform_initializer(seed=self.params["random_seed"]))
+        return score
+
+
+class RankNet(BaseRankModel):
+
+    def __init__(self, model_name, params, logger, training=True):
