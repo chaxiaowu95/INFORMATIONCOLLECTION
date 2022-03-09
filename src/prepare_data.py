e3e3e57906b34713feef5faded64cfb73138d157
chaxiaowu95 <104088657+chaxiaowu95@users.noreply.github.com>
2022-03-09 11:59:10 +0000

    JGD

--- src/prepare_data.py
+++ src/prepare_data.py
@@ -5,4 +5,4 @@ import numpy as np
 
 label_file_pat = "../data/processed/%s_label.npy"
 group_file_pat = "../data/processed/%s_group.npy"
-feature_file_pat = "../data/processed/%s_feat
+feature_file_pat = "../data/processed/%s_feature.npy
