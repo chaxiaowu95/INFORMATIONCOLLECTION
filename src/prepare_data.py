441ca411dfed0cd08a23a8693037dfa009357562
chaxiaowu95 <104088657+chaxiaowu95@users.noreply.github.com>
2022-03-09 19:33:36 +0000

    Update translation files

--- src/prepare_data.py
+++ src/prepare_data.py
@@ -5,4 +5,7 @@ import numpy as np
 
 label_file_pat = "../data/processed/%s_label.npy"
 group_file_pat = "../data/processed/%s_group.npy"
-feature_file_pat = "../data/processed/%s_feature.npy
+feature_file_pat = "../data/processed/%s_feature.npy"
+
+
+def
