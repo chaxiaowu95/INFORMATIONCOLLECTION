e4f2dc14d0f28fa701a54f7dcba2897b29c90c3c
chaxiaowu95 <104088657+chaxiaowu95@users.noreply.github.com>
2022-04-22 18:54:21 +0000

    Add LICENSE.txt file

--- src/tf_common/nadam.py
+++ src/tf_common/nadam.py
@@ -5,3 +5,10 @@ from tensorflow.python.eager import context
 from tensorflow.python.framework import ops
 from tensorflow.python.ops import array_ops
 from tensorflow.python.ops import control_flow_ops
+from tensorflow.python.ops import math_ops
+from tensorflow.python.ops import resource_variable_ops
+from tensorflow.python.ops import state_ops
+from tensorflow.python.ops import variable_scope
+from tensorflow.python.training import optimizer
+from tensorflow.python.training import training_ops
+
