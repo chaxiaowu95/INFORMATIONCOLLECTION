e87557c037494561da27c9f4f400f82953e4d9cf
chaxiaowu95 <104088657+chaxiaowu95@users.noreply.github.com>
2022-04-22 18:59:51 +0000

    removed unused packages

--- src/tf_common/nadam.py
+++ src/tf_common/nadam.py
@@ -12,3 +12,10 @@ from tensorflow.python.ops import variable_scope
 from tensorflow.python.training import optimizer
 from tensorflow.python.training import training_ops
 
+
+class NadamOptimizer(optimizer.Optimizer):
+    def __init__(self, learning_rate=0.002, beta1=0.9, beta2=0.999, epsilon=1e-8,
+                 schedule_decay=0.004, use_locking=False, name="Nadam"):
+        super(NadamOptimizer, self).__init__(use_locking, name)
+        self._lr = learning_rate
+        self._beta1 = beta1
