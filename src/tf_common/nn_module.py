dc05c87e3206ce799ac094bf53d95aba9f038f8d
chaxiaowu95 <104088657+chaxiaowu95@users.noreply.github.com>
2022-05-06 09:44:33 +0000

    conf update

--- src/tf_common/nn_module.py
+++ src/tf_common/nn_module.py
@@ -18,3 +18,8 @@ def batch_normalization(x, training, name):
 #### Step 1
 def embed(x, size, dim, seed=0, flatten=False, reduce_sum=False):
     # std = np.sqrt(2 / dim)
+    std = 0.001
+    minval = -std
+    maxval = std
+    emb = tf.Variable(tf.random_uniform([size, dim], minval, maxval, dtype=tf.float32, seed=seed))
+    # None * max_seq_len * embed_dim
