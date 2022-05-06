837fc2ffb5ee216b27c666393fbf5c02d2179422
chaxiaowu95 <104088657+chaxiaowu95@users.noreply.github.com>
2022-05-06 09:29:36 +0000

    docs: add readme

--- src/tf_common/nn_module.py
+++ src/tf_common/nn_module.py
@@ -11,3 +11,10 @@ def batch_normalization(x, training, name):
     # with tf.variable_scope(name, reuse=)
     bn_train = tf.layers.batch_normalization(x, training=True, reuse=None, name=name)
     bn_inference = tf.layers.batch_normalization(x, training=False, reuse=True, name=name)
+    z = tf.cond(training, lambda: bn_train, lambda: bn_inference)
+    return z
+
+
+#### Step 1
+def embed(x, size, dim, seed=0, flatten=False, reduce_sum=False):
+    # std = np.sqrt(2 / dim)
