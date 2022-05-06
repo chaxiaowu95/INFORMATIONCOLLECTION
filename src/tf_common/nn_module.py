4675eec84ac84a34697023c6ccd0778bfa6cac8b
chaxiaowu95 <104088657+chaxiaowu95@users.noreply.github.com>
2022-05-06 06:40:35 +0000

    Update sidebar.html

--- src/tf_common/nn_module.py
+++ src/tf_common/nn_module.py
@@ -2,3 +2,12 @@
 
 import numpy as np
 import tensorflow as tf
+
+"""
+https://explosion.ai/blog/deep-learning-formula-nlp
+embed -> encode -> attend -> predict
+"""
+def batch_normalization(x, training, name):
+    # with tf.variable_scope(name, reuse=)
+    bn_train = tf.layers.batch_normalization(x, training=True, reuse=None, name=name)
+    bn_inference = tf.layers.batch_normalization(x, training=False, reuse=True, name=name)
