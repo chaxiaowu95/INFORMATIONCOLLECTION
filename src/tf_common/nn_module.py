9f32870968af7a3262df14e0f8b07c8f08d748cc
chaxiaowu95 <104088657+chaxiaowu95@users.noreply.github.com>
2022-06-07 18:25:09 +0000

    debug info

--- src/tf_common/nn_module.py
+++ src/tf_common/nn_module.py
@@ -345,3 +345,11 @@ def _resnet_branch_mode1(x, hidden_units, dropouts, training, seed=0):
     x2 = tf.layers.BatchNormalization()(x2, training=training)
     # x2 = batch_normalization(x2, training=training, name=name + "-" + str(2))
     x2 = tf.nn.relu(x2)
+    x2 = tf.layers.Dropout(dr2, seed=seed * 2)(x2, training=training) if dr2 > 0 else x2
+
+    x2 = tf.layers.Dense(h3, kernel_initializer=tf.glorot_uniform_initializer(seed=seed * 4), dtype=tf.float32,
+                         bias_initializer=tf.zeros_initializer())(x2)
+    x2 = tf.layers.BatchNormalization()(x2, training=training)
+    # x2 = batch_normalization(x2, training=training, name=name + "-" + str(3))
+
+    return x2
