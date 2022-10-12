c62620c9d207ac903a28cb6b28eaeba0d1c5aaa7
chaxiaowu95 <104088657+chaxiaowu95@users.noreply.github.com>
2022-10-12 14:35:22 +0000

    Create Readme.MD

--- src/utils.py
+++ src/utils.py
@@ -17,4 +17,4 @@ def _get_logger(logdir, logname, loglevel=logging.INFO):
     fmt = "[%(asctime)s] %(levelname)s: %(message)s"
     formatter = logging.Formatter(fmt)
 
-    handler = logging.handlers.Rotating
+    handler = logging.handlers.RotatingFileHa
